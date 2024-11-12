1cd608433faaf9bc4ada180d6721dd721984fdd1
Nandriao <binonandriao.2001@gmail.com>
2024-11-12 15:21:24 +0200

    fixing find user error

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -15,7 +15,7 @@ export async function POST(request: Request) {
     }
 
     // Find user
-    const user = await prisma.user.findUnique({
+    const user = await prisma.user.findFirst({
       where: { phoneNumber },
     });
 
