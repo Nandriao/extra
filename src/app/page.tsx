696e7867ab118ca824db240ae92e4070f96688d2
Nandriao <binonandriao.2001@gmail.com>
2024-11-13 16:04:01 +0200

    fix redirect layout sistem on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,165 +46,170 @@ export default function Home() {
 
   const { isLoading, isAuthenticated } = useAuth();
 
-  React.useEffect(() => {
-    if (!isLoading && !isAuthenticated) {
+  if (isLoading) {
+    return <Loading />;
+  } else {
+    if (!isAuthenticated) {
       router.push("/authentication/login");
+    } else {
+      return (
+        <main className="flex flex-col mb-3 min-h-screen">
+          <div className="flex flex-col gap-3">
+            <Header />
+            <div className="flex flex-col mt-3 gap-3">
+              <div className="flex px-3  flex-row items-center w-full">
+                <Image
+                  src={Coins}
+                  alt="Balance"
+                  className="w-[150px] -ml-2  mt-3"
+                />
+                <div className="flex flex-col w-full">
+                  <div className="flex flex-row items-center justify-between w-full transition-all">
+                    <p className="text-xl text-gray-500 font-[poppins]">
+                      Saldo
+                    </p>
+
+                    <Button
+                      className="bg-transparent border-none shadow-none"
+                      variant={"outline"}
+                      onClick={() => {
+                        setShow(!show);
+                      }}
+                    >
+                      {show ? (
+                        <AiOutlineEye color={Colors.GRAY[500]} size={25} />
+                      ) : (
+                        <AiOutlineEyeInvisible
+                          color={Colors.GRAY[500]}
+                          size={25}
+                        />
+                      )}
+                    </Button>
+                  </div>
+                  <p className="text-2xl transition-all flex flex-row text-gray-600 font-semibold font-[poppins]">
+                    {show ? "0,00" : "****"} MZN
+                  </p>
+                </div>
+              </div>
+
+              <div className="flex px-3  flex-row justify-between w-full items-center">
+                <p className="text-gray-500">Operações</p>
+                <ChevronRight color={Colors.GRAY[500]} size={25} />
+              </div>
+
+              <ScrollArea className="w-full">
+                <div className="flex px-3 w-max gap-2">
+                  <OperationCard
+                    href=""
+                    imageSrc={Bronze}
+                    imageAlt="Deposit"
+                    title="Bronze"
+                  />
+
+                  <OperationCard
+                    href="/dashboard/plans"
+                    imageSrc={Send}
+                    imageAlt="plans"
+                    title="Atualizar"
+                  />
+
+                  <OperationCard
+                    href="/dashboard/withdraw"
+                    imageSrc={Withdraw}
+                    imageAlt="Withdraw"
+                    title="Levantar"
+                  />
+                </div>
+                <ScrollBar orientation="horizontal" />
+              </ScrollArea>
+
+              <div className="flex flex-col gap-2">
+                <div className="flex px-3  flex-row justify-between w-full items-center">
+                  <p className="text-gray-500">Planos e comições</p>
+                  <ChevronRight color={Colors.GRAY[500]} size={25} />
+                </div>
+
+                <ScrollArea className="w-full">
+                  <div className="flex px-3 pb-2 w-max gap-2">
+                    <PlansSmall
+                      active={active === 1 ? 1 : 0}
+                      name="Bronze"
+                      logo={Bronze}
+                      clickFunction={() => setActive(1)}
+                    />
+
+                    <PlansSmall
+                      active={active === 2 ? 1 : 0}
+                      name="Silver"
+                      logo={Silver}
+                      clickFunction={() => setActive(2)}
+                    />
+
+                    <PlansSmall
+                      active={active === 3 ? 1 : 0}
+                      name="Gold"
+                      logo={Gold}
+                      clickFunction={() => setActive(3)}
+                    />
+
+                    <PlansSmall
+                      active={active === 4 ? 1 : 0}
+                      name="Platinum"
+                      logo={Platinum}
+                      clickFunction={() => setActive(4)}
+                    />
+
+                    <PlansSmall
+                      active={active === 5 ? 1 : 0}
+                      name="Emerald"
+                      logo={Emerald}
+                      clickFunction={() => setActive(5)}
+                    />
+
+                    <PlansSmall
+                      active={active === 6 ? 1 : 0}
+                      name="Diamond"
+                      logo={Diamond}
+                      clickFunction={() => setActive(6)}
+                    />
+                  </div>
+                  <ScrollBar orientation="horizontal" />
+                </ScrollArea>
+              </div>
+
+              <div className="flex px-3  flex-col gap-2 flex-wrap ">
+                <ItemsCard
+                  logo={Aliexpress}
+                  logoAlt="Aliexpress"
+                  ranquing={Bronze}
+                  ranquingAlt="Bronze"
+                  value="25.00"
+                  category="500"
+                />
+
+                <ItemsCard
+                  logo={Amazon}
+                  logoAlt="Amazon"
+                  ranquing={Bronze}
+                  ranquingAlt="Bronze"
+                  value="25.00"
+                  category="500"
+                />
+
+                <ItemsCard
+                  logo={Magalu}
+                  logoAlt="Magalu"
+                  ranquing={Bronze}
+                  ranquingAlt="Bronze"
+                  value="25.00"
+                  category="500"
+                />
+              </div>
+            </div>
+            <Footer />
+          </div>
+        </main>
+      );
     }
-  }, [isLoading, isAuthenticated, router]);
-
-  if (isLoading) return <Loading />;
-
-  return (
-    <main className="flex flex-col mb-3 min-h-screen">
-      <div className="flex flex-col gap-3">
-        <Header />
-        <div className="flex flex-col mt-3 gap-3">
-          <div className="flex px-3  flex-row items-center w-full">
-            <Image
-              src={Coins}
-              alt="Balance"
-              className="w-[150px] -ml-2  mt-3"
-            />
-            <div className="flex flex-col w-full">
-              <div className="flex flex-row items-center justify-between w-full transition-all">
-                <p className="text-xl text-gray-500 font-[poppins]">Saldo</p>
-
-                <Button
-                  className="bg-transparent border-none shadow-none"
-                  variant={"outline"}
-                  onClick={() => {
-                    setShow(!show);
-                  }}
-                >
-                  {show ? (
-                    <AiOutlineEye color={Colors.GRAY[500]} size={25} />
-                  ) : (
-                    <AiOutlineEyeInvisible color={Colors.GRAY[500]} size={25} />
-                  )}
-                </Button>
-              </div>
-              <p className="text-2xl transition-all flex flex-row text-gray-600 font-semibold font-[poppins]">
-                {show ? "0,00" : "****"} MZN
-              </p>
-            </div>
-          </div>
-
-          <div className="flex px-3  flex-row justify-between w-full items-center">
-            <p className="text-gray-500">Operações</p>
-            <ChevronRight color={Colors.GRAY[500]} size={25} />
-          </div>
-
-          <ScrollArea className="w-full">
-            <div className="flex px-3 w-max gap-2">
-              <OperationCard
-                href=""
-                imageSrc={Bronze}
-                imageAlt="Deposit"
-                title="Bronze"
-              />
-
-              <OperationCard
-                href="/dashboard/plans"
-                imageSrc={Send}
-                imageAlt="plans"
-                title="Atualizar"
-              />
-
-              <OperationCard
-                href="/dashboard/withdraw"
-                imageSrc={Withdraw}
-                imageAlt="Withdraw"
-                title="Levantar"
-              />
-            </div>
-            <ScrollBar orientation="horizontal" />
-          </ScrollArea>
-
-          <div className="flex flex-col gap-2">
-            <div className="flex px-3  flex-row justify-between w-full items-center">
-              <p className="text-gray-500">Planos e comições</p>
-              <ChevronRight color={Colors.GRAY[500]} size={25} />
-            </div>
-
-            <ScrollArea className="w-full">
-              <div className="flex px-3 pb-2 w-max gap-2">
-                <PlansSmall
-                  active={active === 1 ? 1 : 0}
-                  name="Bronze"
-                  logo={Bronze}
-                  clickFunction={() => setActive(1)}
-                />
-
-                <PlansSmall
-                  active={active === 2 ? 1 : 0}
-                  name="Silver"
-                  logo={Silver}
-                  clickFunction={() => setActive(2)}
-                />
-
-                <PlansSmall
-                  active={active === 3 ? 1 : 0}
-                  name="Gold"
-                  logo={Gold}
-                  clickFunction={() => setActive(3)}
-                />
-
-                <PlansSmall
-                  active={active === 4 ? 1 : 0}
-                  name="Platinum"
-                  logo={Platinum}
-                  clickFunction={() => setActive(4)}
-                />
-
-                <PlansSmall
-                  active={active === 5 ? 1 : 0}
-                  name="Emerald"
-                  logo={Emerald}
-                  clickFunction={() => setActive(5)}
-                />
-
-                <PlansSmall
-                  active={active === 6 ? 1 : 0}
-                  name="Diamond"
-                  logo={Diamond}
-                  clickFunction={() => setActive(6)}
-                />
-              </div>
-              <ScrollBar orientation="horizontal" />
-            </ScrollArea>
-          </div>
-
-          <div className="flex px-3  flex-col gap-2 flex-wrap ">
-            <ItemsCard
-              logo={Aliexpress}
-              logoAlt="Aliexpress"
-              ranquing={Bronze}
-              ranquingAlt="Bronze"
-              value="25.00"
-              category="500"
-            />
-
-            <ItemsCard
-              logo={Amazon}
-              logoAlt="Amazon"
-              ranquing={Bronze}
-              ranquingAlt="Bronze"
-              value="25.00"
-              category="500"
-            />
-
-            <ItemsCard
-              logo={Magalu}
-              logoAlt="Magalu"
-              ranquing={Bronze}
-              ranquingAlt="Bronze"
-              value="25.00"
-              category="500"
-            />
-          </div>
-        </div>
-        <Footer />
-      </div>
-    </main>
-  );
+  }
 }
